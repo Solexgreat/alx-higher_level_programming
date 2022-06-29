4ce2a0314b5e34a3626891804bcae800e77a3260
root <Solexgreat>
2022-06-29 00:26:33 +0000

    ByteCode -> Python #2

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if (a < b):
+        return (c)
+    elif (c > b):
+        return (a + b)
+    else:
+        return (a * b - c)
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
