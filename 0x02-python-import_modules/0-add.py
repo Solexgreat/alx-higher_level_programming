131f0f1a4b2bbd35283b58cf7114c1ba515abd7a
root <Solexgreat>
2022-06-30 17:18:40 +0000

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-import sys 
-#path = os.path.dirname(os.path.abspath(__file__))
+from add_0 import add
 
-sys.path.insert(0, '/root/als-higher_level_programming/0x01-python-if_else_loops_functions')
-
-print(sys.path)
-print("*"*50)
 a = 1
 b = 2
-add = 10-add.add(a, b)
-print(f"{a} + {b} = {add}")
+c = add(a, b)
+print(f"{a} + {b} = {c}")
