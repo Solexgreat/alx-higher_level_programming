f3da43754fae9563638ee7ca03820ea7f65e8635
root <Solexgreat>
2022-06-30 23:15:43 +0000

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = len(sys.argv) - 1
     tot = 0
-    if n != 0:
     for i in sys.argv:
         if i != sys.argv[0]:
             tot += int(i)
-    else:
     print(tot)    
