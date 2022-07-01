8c50167a4a2f69b8ed603d918725485819fb1610
root <Solexgreat>
2022-07-01 01:50:50 +0000

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
+    from calculator_1 import add, sub, mul, div
     n = len(sys.argv) - 1
+    op = sys.argv[2]
+    a = sys.argv[1]
+    b = sys.argv[3]
     if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    else:
-        if sys.argv[2] == '+':
-        a = sys.argv[1]
-        b = sys.argv[3]
+    elif op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
         sys.exit(0)
-        elif sys.argv == '-':
-        a = sys.argv[1]
-        b = sys.argv[3]
+    elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
         sys.exit(0)
-        elif sys.argv == '*':
-        a = sys.argv[1]
-        b = sys.argv[3]
+     elif op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
         sys.exit(0)
-        elif sys.argv == '/':
-        a = sys.argv[1]
-        b = sys.argv[3]
+     elif op == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
         sys.exit(0)
-        else:
+     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
