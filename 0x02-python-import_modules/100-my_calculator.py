6e797bb21348b0bba67a1567a255b7ee94b72dc8
root <Solexgreat>
2022-07-01 10:00:56 +0000

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,24 +3,25 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     n = len(sys.argv) - 1
-    op = sys.argv[2]
-    a = sys.argv[1]
-    b = sys.argv[3]
     if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif op == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-        sys.exit(0)
-    elif op == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-        sys.exit(0)
-     elif op == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-        sys.exit(0)
-     elif op == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
-        sys.exit(0)
-     else:
+    op = sys.argv[2]
+    a = sys.argv[1]
+    b = sys.argv[3]
+    elif n == 3:
+        if op == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+            sys.exit(0)
+        elif op == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+            sys.exit(0)
+        elif op == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+            sys.exit(0)
+        elif op == '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
+            sys.exit(0)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
