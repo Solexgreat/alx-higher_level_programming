d6c3a8b5978ee84aeda6aaea819af79370951fd7
root <Solexgreat>
2022-07-01 01:33:49 +0000

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,27 +7,26 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
-        for arg in sys.argv:
-            if sys.argv[2] == '+':
-                a = sys.argv[1]
-                b = sys.argv[3]
-                print("{} + {} = {}".format(a, b, add(a, b)))
-                sys.exit(0)
-            elif sys.argv == '-':
-                a = sys.argv[1]
-                b = sys.argv[3]
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-                sys.exit(0)
-            elif sys.argv == '*':
-                a = sys.argv[1]
-                b = sys.argv[3]
-                print("{} * {} = {}".format(a, b, mul(a, b)))
-                sys.exit(0)
-            elif sys.argv == '/':
-                a = sys.argv[1]
-                b = sys.argv[3]
-                print("{} / {} = {}".format(a, b, div(a, b)))
-                sys.exit(0)
-            else:
-                print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit(1)
+        if sys.argv[2] == '+':
+        a = sys.argv[1]
+        b = sys.argv[3]
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        sys.exit(0)
+        elif sys.argv == '-':
+        a = sys.argv[1]
+        b = sys.argv[3]
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        sys.exit(0)
+        elif sys.argv == '*':
+        a = sys.argv[1]
+        b = sys.argv[3]
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+        sys.exit(0)
+        elif sys.argv == '/':
+        a = sys.argv[1]
+        b = sys.argv[3]
+        print("{} / {} = {}".format(a, b, div(a, b)))
+        sys.exit(0)
+        else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
