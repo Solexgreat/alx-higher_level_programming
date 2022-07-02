3849526e60bc562e180fd183410e957f6bab62bf
root <Solexgreat>
2022-07-02 21:22:08 +0000

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,7 @@ def no_c(my_string):
     j = 0
 
     for i in range(len(my_string)):
-        if i == 'c' or i == 'C':
+        if my_string[i] == 'c' or my_string[i] == 'C':
             newSt = newSt[:(i - j)] + my_string[(i + 1):]
             j += 1
 
