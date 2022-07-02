26aa5d7f316d83b803405d19fe8712c026030eed
root <Solexgreat>
2022-07-02 11:54:38 +0000

    Print the number

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 and idx > len(my_list) - 1:
+    lenght = len(my_list) - 1
+    if idx < 0 and idx > lenght:
         return None
     for i in range(len(my_list)):
         if i == idx:   
-    return (my_list[i])
+            return (my_list[i])
