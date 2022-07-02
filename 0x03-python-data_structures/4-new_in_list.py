7d85bfbb74347b836213ac9d6c15c489c372d9bf
root <Solexgreat>
2022-07-02 13:41:33 +0000

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     my_listd = my_list[:]
+
     if idx < 0:
         return (my_list)
+
     if idx > len(my_list) - 1:
         return (my_list)
+
     my_listd[idx] = element
 
     return (my_listd)
