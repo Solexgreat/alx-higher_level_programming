6c520e33f62080dea5320eb4e7c278ef7e21b9a8
root <Solexgreat>
2022-07-09 02:35:01 +0000

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace)
-    num = lambda x: x if x != search else replace   
+def search_replace(my_list, search, replace):
+    num = (lambda x: x if x != search else replace)
     return list(map(num, my_list))
