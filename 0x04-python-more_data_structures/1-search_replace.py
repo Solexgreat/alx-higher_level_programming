11da36dbd6b2eb8b0df5fba3cb1680e9c71c3a67
root <Solexgreat>
2022-07-09 02:19:17 +0000

    More data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!usr/bin/python3
-def search_replace(my_list, search, replace):
-    #def s_r_elm(lm):
-        #return (lm if lm != search else replace)
+def search_replace(my_list, search, replace)
     num = lambda x: x if x != search else replace   
     return list(map(num, my_list))
