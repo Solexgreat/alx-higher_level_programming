adcb16d60203afb49931633d48fcdbd6f4649578
root <Solexgreat>
2022-08-03 16:21:22 +0000

    done task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,19 @@ list and save them to a json file
 """
 import sys
 import os.path
-import save_to_json_file from 5-save_to_json_file.py
-import load_from_json_file from 6-load_from_json_file.py
+
+
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 mylist = []
 
 if os.path.exists("add_item.json"):
-    load_from_json_file("add_item.json")
+    load_file("add_item.json")
 
 for i in range(len(sys.argv - 1)):
     if i != 0:
         mylist.append(sys.argv[i])
 
-save_to_json_file(mylist, "add_item.json")
+save_file(mylist, "add_item.json")
