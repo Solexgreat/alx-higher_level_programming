af16482df3d92421b6aaca121b0d1401a7c0106c
Solexgreat <Solomongodkid>
2022-10-04 18:59:59 -0700

    first five task done

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+P
