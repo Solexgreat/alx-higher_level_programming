d726d6c629436ca2f87aa356e458c435b3eafc5d
Solexgreat <Solomongodkid>
2022-10-08 16:03:29 -0700

    task done

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db=MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                        db=sys.argv[3], port=3306)
+
+    C=db.cursor()
+    C.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = C.fetchall()
+
+    for state in states:
+        print(state)
+
